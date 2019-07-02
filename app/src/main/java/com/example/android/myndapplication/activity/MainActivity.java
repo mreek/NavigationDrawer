41f05458762d341175186cee8bda2b18d6bf3381
Olcay Ertaş <olcayertas@gmail.com>
2019-07-02 12:50:15 +0300

    Connected drawer menu and bottom navigation.

--- app/src/main/java/com/example/android/myndapplication/activity/MainActivity.java
+++ app/src/main/java/com/example/android/myndapplication/activity/MainActivity.java
@@ -4,9 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import android.widget.ImageButton;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -29,6 +27,12 @@ public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    private BottomNavigationView bottomNavigationView;
+    private MainFragment mainFragment = MainFragment.newInstance();
+    private AccountFragment accountFragment = AccountFragment.newInstance();
+    private SellFragment sellFragment = SellFragment.newInstance();
+    private ChatFragment chatFragment = ChatFragment.newInstance();
+    private NotificationFragment notificationFragment = NotificationFragment.newInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,68 +47,50 @@ public class MainActivity extends AppCompatActivity
         drawer = findViewById(R.id.drawer_layout);
 
         ImageButton menuRight = findViewById(R.id.leftRight);
-        menuRight.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (drawer.isDrawerOpen(GravityCompat.START)) {
-                    drawer.closeDrawer(GravityCompat.START);
-                } else {
-                    drawer.openDrawer(GravityCompat.START);
-                }
+        menuRight.setOnClickListener(v -> {
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                drawer.openDrawer(GravityCompat.START);
             }
         });
 
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        bottomNavigationView = findViewById(R.id.navigation);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
-
-        bottomNavigationView.setOnNavigationItemSelectedListener
-                (new BottomNavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                        Fragment selectedFragment = null;
-                        switch (item.getItemId()) {
-                            case R.id.action_item1:
-                                selectedFragment = MainFragment.newInstance();
-                                // Log.v(TAG, "lwl" );
-                                break;
-                            case R.id.action_item2:
-                                selectedFragment = AccountFragment.newInstance();
-                                //Log.v(TAG, "tani" );
-                                break;
-                            case R.id.action_item3:
-                                selectedFragment = SellFragment.newInstance();
-                                //Log.v(TAG, "talet" );
-                                break;
-
-                            case R.id.action_item4:
-                                selectedFragment = ChatFragment.newInstance();
-                                //Log.v(TAG, "talet" );
-                                break;
-
-                            case R.id.action_item5:
-                                selectedFragment = NotificationFragment.newInstance();
-                                //Log.v(TAG, "talet" );
-                                break;
-                        }
-                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-                        if (selectedFragment != null) {
-                            transaction.replace(R.id.f_container, selectedFragment);
-                            transaction.commit();
-                        }
-                        return true;
-                    }
-                });
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            switch (item.getItemId()) {
+                case R.id.action_announces:
+                    selectedFragment = mainFragment;
+                    break;
+                case R.id.action_account:
+                    selectedFragment = accountFragment;
+                    break;
+                case R.id.action_sell:
+                    selectedFragment = sellFragment;
+                    break;
+                case R.id.action_chat:
+                    selectedFragment = chatFragment;
+                    break;
+                case R.id.action_notifications:
+                    selectedFragment = notificationFragment;
+                    break;
+            }
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            if (selectedFragment != null) {
+                transaction.replace(R.id.f_container, selectedFragment);
+                transaction.commit();
+            }
+            return true;
+        });
 
         //Manually displaying the first fragment - one time only
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.f_container, MainFragment.newInstance());
+        transaction.replace(R.id.f_container, mainFragment);
         transaction.commit();
-
-        //Used to select an item programmatically
-        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
     }
 
     @Override
@@ -119,24 +105,13 @@ public class MainActivity extends AppCompatActivity
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        return true;
     }
 
     @Override
@@ -144,44 +119,30 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawers();
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
 
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-
-        if (id == R.id.nav_camera) {
-            // appBarTV.setText("Main Page");
-            MainFragment fragment = new MainFragment();
-            ft.replace(R.id.f_container, fragment);
-            ft.commit();
-
-        } else if (id == R.id.nav_gallery) {
-            //appBarTV.setText("Fragment With Tabs");
-            ChatFragment fragment = new ChatFragment();
-            ft.replace(R.id.f_container, fragment);
-            ft.commit();
-
-        } else if (id == R.id.nav_slideshow) {
-            //appBarTV.setText("Slideshow Page");
-            //AccountFragment
-            //Toast.makeText(this, "Slideshow", Toast.LENGTH_SHORT).show();
-            AccountFragment fragmentTab = new AccountFragment();
-            ft.replace(R.id.f_container, fragmentTab);
-            ft.commit();
-        } else if (id == R.id.nav_manage) {
-            //appBarTV.setText("Tools Page");
-            // Toast.makeText(this, "Contactez_nous Activity", Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.nav_share) {
-            //appBarTV.setText("Share Page");
-            Toast.makeText(this, "Partager", Toast.LENGTH_SHORT).show();
-
-        } else if (id == R.id.nav_send) {
-            //appBarTV.setText("Send");
-            Toast.makeText(this, "Rate 5 stars", Toast.LENGTH_SHORT).show();
+        switch (item.getItemId()) {
+            case R.id.nav_announces:
+                bottomNavigationView.setSelectedItemId(R.id.action_announces);
+                ft.replace(R.id.f_container, mainFragment);
+                break;
+            case R.id.nav_account:
+                bottomNavigationView.setSelectedItemId(R.id.action_account);
+                ft.replace(R.id.f_container, accountFragment);
+                break;
+            case R.id.nav_sell:
+                bottomNavigationView.setSelectedItemId(R.id.action_sell);
+                ft.replace(R.id.f_container, sellFragment);
+                break;
+            case R.id.nav_chat:
+                bottomNavigationView.setSelectedItemId(R.id.action_chat);
+                ft.replace(R.id.f_container, chatFragment);
+                break;
+            case R.id.nav_notifications:
+                bottomNavigationView.setSelectedItemId(R.id.action_notifications);
+                ft.replace(R.id.f_container, notificationFragment);
+                break;
         }
 
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
+        ft.commit();
         return true;
     }
 }
